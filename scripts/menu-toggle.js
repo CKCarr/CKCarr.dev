0b557da78b7dd9ee05005e45438002d7ccf7b72d
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-26 02:27:53 -0600

    fix navbar

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -4,3 +4,26 @@ const navLinks = document.querySelector('.nav-links');
 logo.addEventListener('click', () => {
     navLinks.classList.toggle('show-links');
 });
+
+
+/* navbar attempt */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
